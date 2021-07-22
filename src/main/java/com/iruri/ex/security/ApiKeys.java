a38cb538278ae3e049ea494a856148528fa57593
leeyunji98 <twister1114@gmail.com>
2021-07-22 09:42:37 +0900

    com.iruri.ex.security

---
+++ src/main/java/com/iruri/ex/security/ApiKeys.java
@@ -0,0 +1,9 @@
+package com.iruri.ex.security;
+
+public class ApiKeys {
+    public static String G_CLIENT_ID = "245140385098-t3nm98ju7aq730v7mj0non5br41l51s3.apps.googleusercontent.com";
+    public static String G_CLIENT_SECRET = "7TikRAw0PtSawGMLwyOhCqLn";
+    public static String K_CLIENT_ID = "d575d2c5d3ba998a1b5e51f9f6a24072";
+    public static String N_CLIENT_ID = "C0EmkdVZxGINvoALrSGN";
+    public static String N_CLIENT_SECRET = "fWpmifbWYr";
+}
