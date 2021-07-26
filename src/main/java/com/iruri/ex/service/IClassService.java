8fb5393105e760c91e3eacd02e9be05c1a467b1c
dlskfo306 <dlskfo306@gmail.com>
2021-07-26 20:31:09 +0900

    210726-페이징처리 (화면에 아직 안뜸), 챌린지 메인 리스트 DB불러오기 (완료), 챌린지 개설 폼 달력부분 datepicker사용(오늘 이전날짜, 시작날짜 이전 선택 불가 적용)(쿼리버전문제 같음 안뜸)

--- src/main/java/com/iruri/ex/service/IClassService.java
+++ src/main/java/com/iruri/ex/service/IClassService.java
@@ -2,6 +2,7 @@ package com.iruri.ex.service;
 
 import java.util.List;
 
+import com.iruri.ex.page.Criteria;
 import com.iruri.ex.vo.IClassVO;
 
 public interface IClassService {
@@ -12,6 +13,17 @@ public interface IClassService {
     
     int classcount(int userId);
     
+    
+    //챌린지 메인 리스트
+    List<IClassVO> challengeList();
+
+    
     //챌린지 개설 폼
     void insertChallenge(IClassVO iClassVO);
+    
+    
+    //페이징 처리
+    public List<IClassVO> getList(Criteria criteria);
+    public int getTotal(Criteria cri);
+    
 }
